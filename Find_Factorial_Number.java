2d1a41e8e6f49d82bb499e6533991825bc6c0948
Nitish <nitishnbt0452@gmail.com>
2025-04-29 23:26:31 +0530

    c

--- Find_Factorial_Number.java
+++ Find_Factorial_Number.java
@@ -1,5 +1,22 @@
 package Bootcamp1st.Practice_Questions;
 
+import java.util.Scanner;
+
 public class Find_Factorial_Number {
-    
+   public static int Factorial(int n){
+       if (n == 0 || n == 1)
+           return 1;
+       else
+           return n * Factorial(n - 1);
+
+   }
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a Number ");
+        int num =sc.nextInt();
+
+       int result= Factorial(num);
+        System.out.println(result);
+    }
 }
